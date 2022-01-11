274ef187bd1403e38101f6752c978a2a22217433
Jonas Dammen <jonas.dammen@capgemini.com>
2022-01-11 21:46:33 +0100

    update imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import { useEffect } from 'react';
 import Header from './components/Header';
-import { Gallery } from './components/Gallery/Gallery';
-import { AboutUs } from './components/AboutUs/AboutUs';
-import { pingServer } from './utils.js';
+import { pingServer } from './utils/utils';
 import './App.css';
 const App = () => {
-  const aboutUs = false;
   useEffect(() => {
     pingServer();
   }, []);

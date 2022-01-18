7aea366e02f4ed76cefe9ae76047dd64986dbfca
Jonas Dammen <jonas.dammen@capgemini.com>
2022-01-18 21:33:05 +0100

    div fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import Header from './components/Header';
 import './App.scss';
-import  { Models } from "./components/Models";
-import {FunctionComponent} from "react";
+import { Models } from './components/Models';
+import { FunctionComponent } from 'react';
+import './appInsights/appInsights';
 
 const App: FunctionComponent = () => {
     return (

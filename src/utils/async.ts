52a11c44422f692564877fd27b657377e7844133
Hege Haavaldsen <hege.haavaldsen@nav.no>
2022-01-22 11:06:46 +0100

    Send correct model to model-endpoint

--- src/utils/async.ts
+++ src/utils/async.ts
@@ -11,7 +11,7 @@ export async function fetchImagePaths(model: string) {
        {
           method: 'POST',
           headers: { 'Content-Type': 'application/json'},
-          body: JSON.stringify({ "model": "portrait"})
+          body: JSON.stringify({ "model": model})
        }
    ).then(response => response.json())
 }

8cfc9580808d42b345a6628f7dd34c2076e0e9f3
Jonas Dammen <jonas.dammen@capgemini.com>
2022-01-22 17:57:05 +0100

    use after instead of excessive div

--- src/components/Models/index.tsx
+++ src/components/Models/index.tsx
@@ -1,36 +1,39 @@
-import {FunctionComponent, useEffect, useState} from "react";
-import {fetchModels} from "../../utils/async";
-import {Model} from "../../utils/types";
-import './models.scss'
-import {Link} from "react-router-dom";
+import { FunctionComponent, useEffect, useState } from 'react';
+import { fetchModels } from '../../utils/async';
+import { Model } from '../../utils/types';
+import { Link } from 'react-router-dom';
+import './models.scss';
 
 interface ModelProps {
     displayName: string;
     name: string;
 }
-const ModelLink: FunctionComponent<ModelProps> = ({displayName, name}) => {
+const ModelLink: FunctionComponent<ModelProps> = ({ displayName, name }) => {
     return (
-    <div className="modelContainer">
-            <Link className="modelLink" to={`/galleri/${name}`}>{displayName}</Link>
-            <div className="modelLinkLine"/>
-    </div>
-)}
-
+        <div className="modelContainer">
+            <Link className="modelLink" to={`/galleri/${name}`}>
+                {displayName}
+            </Link>
+        </div>
+    );
+};
 
 export const Models: FunctionComponent = () => {
-
-    const [models, setModels] = useState<Model[]>([])
+    const [models, setModels] = useState<Model[]>([]);
     useEffect(() => {
-        fetchModels()
-            .then(response => setModels(response.models))
-    }, [])
+        fetchModels().then((response) => setModels(response.models));
+    }, []);
 
     return (
         <section className="modelsContainer">
             <div className="modelLinksContainer">
-            {models.map((model, index) =>
-                <ModelLink displayName={model.displayName} name={model.name} key={index}/>
-            )}
+                {models.map((model, index) => (
+                    <ModelLink
+                        displayName={model.displayName}
+                        name={model.name}
+                        key={index}
+                    />
+                ))}
             </div>
         </section>
     );

8854f2c7a6d280dafbe24b253d77160c031380b4
Jonas Dammen <jonas.dammen@capgemini.com>
2022-01-22 02:33:08 +0100

    Set up asp.net server

---
+++ ASP.NETCoreWebApplication1/ClientApp/src/utils/utils.js
@@ -0,0 +1,18 @@
+import f from '../images/1024.jpeg';
+export const mockImages = [f, f, f, f, f, f, f, f, f, f];
+
+export const pingServer = () => {
+    fetch(getBaseUrl())
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+};
+
+const runOnLocalServer = false;
+
+export const getBaseUrl = () => {
+    if (runOnLocalServer) {
+        return '';
+    } else {
+        return 'https://glup-kunstig-api.azurewebsites.net/';
+    }
+};

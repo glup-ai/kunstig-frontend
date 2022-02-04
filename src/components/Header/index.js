c98e835a2aa6f500df45089f769aa833591ed50e
Hege Haavaldsen <hege.haavaldsen@nav.no>
2022-02-04 08:04:10 +0100

    Make header more mobile friendly

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,13 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import './header.scss'
 
 export const Header = () => (
     <nav className="headerContainer">
         <Link className="headerTitle" to="/">Kunstig</Link>
-        <Link className="headerLink" to="/galleri">Galleri</Link>
-        <Link className="headerLink" to="/generer">Lag kunst</Link>
-        <Link className="headerLink" to="/om">Om prosjektet</Link>
-
+        <div className="headerLinkContainer">
+            <Link className="headerLink" to="/galleri">Galleri</Link>
+            <Link className="headerLink" to="/generer">Lag kunst</Link>
+            <Link className="headerLink" to="/om">Om prosjektet</Link>
+        </div>
     </nav>
 );

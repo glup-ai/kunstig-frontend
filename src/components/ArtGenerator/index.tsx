b1896bafc9d1b2f996dc49ba638423e512b37db6
Hege Haavaldsen <hege.haavaldsen@nav.no>
2022-02-04 09:00:25 +0100

    Make art generator more mobile friendly

--- src/components/ArtGenerator/index.tsx
+++ src/components/ArtGenerator/index.tsx
@@ -2,97 +2,97 @@ import {FunctionComponent, useCallback, useContext, useEffect, useState} from 'r
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 
-import { saveAs } from 'file-saver';
+import {saveAs} from 'file-saver';
 
 import './artgenerator.scss';
-import { getBaseUrl } from '../../utils/utils.js';
-import { Header } from "../Header";
-import { useParams } from "react-router-dom";
+import {getBaseUrl} from '../../utils/utils.js';
+import {Header} from "../Header";
+import {useParams} from "react-router-dom";
 import {ModelsAsyncContext} from "../../context/ModelAsync";
 import {Spinner} from "../Spinner/Spinner";
 
 export const ArtGenerator: FunctionComponent = () => {
-  const { name } = useParams();
-  const [image, setImage] = useState<string>();
-  const [isLoading, setIsLoading] = useState(true);
-  const [currentModel, setCurrentModel] = useState(name ?? "nonfigurativAbstrusivitet")
-  const {modelsAsyncState} = useContext(ModelsAsyncContext);
+    const {name} = useParams();
+    const [image, setImage] = useState<string>();
+    const [isLoading, setIsLoading] = useState(true);
+    const [currentModel, setCurrentModel] = useState(name ?? "nonfigurativAbstrusivitet")
+    const {modelsAsyncState} = useContext(ModelsAsyncContext);
 
-  const fetchImage = useCallback(() => {
-    setIsLoading(true);
-    fetch(getBaseUrl() + 'generate', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ "model": currentModel })
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw Error(response.statusText);
+    const fetchImage = useCallback(() => {
+        setIsLoading(true);
+        fetch(getBaseUrl() + 'generate', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({"model": currentModel})
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                return response;
+            })
+            .then((response) => response.blob())
+            .then((blob) => {
+                setImage(URL.createObjectURL(blob));
+                setIsLoading(false);
+            });
+    }, [currentModel]);
+
+    useEffect(() => {
+        fetchImage();
+    }, [fetchImage]);
+
+    const downloadImage = (img) => {
+        if (img) {
+            saveAs(img, 'image.jpg');
         }
-        return response;
-      })
-      .then((response) => response.blob())
-      .then((blob) => {
-        setImage(URL.createObjectURL(blob));
-        setIsLoading(false);
-      });
-  }, [currentModel]);
+    };
 
-  useEffect(() => {
-    fetchImage();
-  }, [fetchImage]);
+    const options = modelsAsyncState.data?.map(model => ({value: model.name, label: model.displayName}))
 
-  const downloadImage = (img) => {
-    if (img) {
-      saveAs(img, 'image.jpg');
-    }
-  };
+    return (
+        <>
+            <Header/>
+            <section className="artgeneratorContainer">
+                <div className="artgeneratorDropdownContainer">
+                    {options && <Dropdown
+                        options={options}
+                        onChange={event => setCurrentModel(event.value)}
+                        value={currentModel}
+                        placeholder="Velg en AI-kunstner"
+                        className="artgeneratorDropdown"
+                        controlClassName="artgeneratorDropdownControl"
+                        menuClassName="artgeneratorDropdownMenu"
+                        arrowClassName="artgeneratorDropdownArrow"
+                    />}
+                </div>
+                <div className="artgeneratorImageContainer">
+                    <div className="artgeneratorImage">
+                        {isLoading ? (
+                            <Spinner/>
+                        ) : (
+                            <img alt="AI-generated art by kunstig" src={image}/>
+                        )}
+                    </div>
+                </div>
+                <div className="actionButtonsContainer">
+                    <button
+                        className="actionButtonContainer"
+                        onClick={() => fetchImage()}
+                    >
+                        <p>Generer ny kunst</p>
+                    </button>
 
-  const options = modelsAsyncState.data?.map(model => ({value: model.name, label: model.displayName}))
-
-  return (
-    <>
-      <Header />
-      <section className="artgeneratorContainer">
-        <div className="artgeneratorDropdownContainer">
-          {options && <Dropdown
-              options={options}
-              onChange={event => setCurrentModel(event.value)}
-              value={currentModel}
-              placeholder="Velg en AI-kunstner"
-              className="artgeneratorDropdown"
-              controlClassName="artgeneratorDropdownControl"
-              menuClassName="artgeneratorDropdownMenu"
-              arrowClassName="artgeneratorDropdownArrow"
-          />}
-        </div>
-        <div className="artgeneratorImageContainer">
-          <div className="artgeneratorImage">
-            {isLoading ? (
-              <Spinner/>
-            ) : (
-              <img alt="AI-generated art by kunstig" src={image} />
-            )}
-          </div>
-        </div>
-        <div className="actionButtonsContainer">
-        <button
-            className="actionButtonContainer"
-            onClick={() => fetchImage()}
-          >
-            <p>Generer ny kunst</p>
-          </button>
-
-          <button
-            className="actionButtonContainer"
-            onClick={() => downloadImage(image)}
-          >
-            <p>Last ned bildet</p>
-          </button>
-        </div>
-      </section>
-    </>
-  );
+                    <button
+                        className="actionButtonContainer"
+                        onClick={() => downloadImage(image)}
+                    >
+                        <p>Last ned bildet</p>
+                    </button>
+                </div>
+            </section>
+        </>
+    );
 };
 
 export default ArtGenerator;

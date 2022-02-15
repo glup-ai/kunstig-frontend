50adb8cff5865424732a31c9ab8551a8e36214f5
Hege Haavaldsen <hege.haavaldsen@nav.no>
2022-02-15 20:35:34 +0100

    Add link to gallery for all models

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,6 +5,7 @@ export const Header = () => (
     <nav className="headerContainer">
         <Link className="headerTitle" to="/">Kunstig</Link>
         <div className="headerLinkContainer">
+            <Link className="headerLink" to="/galleri">Galleri</Link>
             <Link className="headerLink" to="/generer">Lag kunst</Link>
             <Link className="headerLink" to="/om">Om prosjektet</Link>
         </div>

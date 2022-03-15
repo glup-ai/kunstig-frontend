3acd0ba0669164c3b4a65dff58e5c2c918450d5c
Hege Haavaldsen <hege.haavaldsen@nav.no>
2022-03-15 19:40:10 +0100

    Use gallery as home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { Header } from './components/Header';
 import './App.scss';
-import { Models } from './components/Models';
 import { FunctionComponent } from 'react';
 import './appInsights/appInsights';
+import {Gallery} from "./components/Gallery";
 
 const App: FunctionComponent = () => {
     return (
         <main className="mainContainer">
-            <Header />
-            <Models />
+            <Gallery />
         </main>
     );
 };
